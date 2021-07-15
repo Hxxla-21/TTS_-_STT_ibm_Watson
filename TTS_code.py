0492c3d8844f8494eb11c0a5e6dac9bee4ca05fb
Hxxla-21 <84406741+Hxxla-21@users.noreply.github.com>
2021-07-15 04:14:58 +0300

    Update TTS_code.py

--- TTS_code.py
+++ TTS_code.py
@@ -2,14 +2,14 @@ import json
 from ibm_watson import TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-api = IAMAuthenticator('9_8pHCLe5Ief6hrWv1QPiWncPmZrnWXoMUd5FH2kA410')
+api = IAMAuthenticator('UtMD_Rm1yhk7T_iZVzk-ryDHbSTC270mW2LMigHXhOmJ')
 
 text2speech= TextToSpeechV1(authenticator=api)
 
 
-text2speech.set_service_url('https://api.eu-gb.text-to-speech.watson.cloud.ibm.com/instances/ce312d68-67a2-4bab-a06c-7c5dc4200d42')
+text2speech.set_service_url('https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/f14a0b3d-3272-47dd-9e49-f78f55bc4a6d')
 
 with open("welcome.mp3","wb") as audiofile:
         audiofile.write(
-            text2speech.synthesize("Hello, My Name is Hala. I'm a software engineering from a University of jeddah ",accept="audio/mp3").get_result().content
+            text2speech.synthesize("Hello, My Name is Hala. I'm a software engineering from a University of jeddah, thank you ",accept="audio/mp3").get_result().content
         )
