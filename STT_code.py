0572ed7273a11a7896f59644d692d62a04a6230d
Hxxla-21 <84406741+Hxxla-21@users.noreply.github.com>
2021-07-15 04:14:26 +0300

    Update STT_code.py

--- STT_code.py
+++ STT_code.py
@@ -2,12 +2,12 @@ import json
 from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-api = IAMAuthenticator('5ryInlRr2O74-b_VOUeJkrpat653l3kLoQWeNjFgAa4I')
+api = IAMAuthenticator('G5cSyxVG132iTdfF9NZFeR8w8c2qBqNRvTR5UoSpKw31')
 
 speech2text= SpeechToTextV1(authenticator=api)
 
 
-speech2text.set_service_url('https://api.au-syd.speech-to-text.watson.cloud.ibm.com/instances/182a22de-7481-4b7f-beb5-5e29c3c6dd0d')
+speech2text.set_service_url('https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/d1e34d94-6127-41fd-9b22-27641ca24bf8')
 
 with open("welcome.mp3","rb") as audiofile:
     
